a1896dff0970d10e7987a998d66be7c412b96412
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-08-18 22:33:20 +0300

    react-build

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+    render(){
+        "use strict";
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight adn</h1>
+                <p>react, redux and router</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
